f370e250192cb11ec286d1e3c9dbcb25011435ef
mesho3620 <44414933+mesho3620@users.noreply.github.com>
2018-10-25 01:34:05 +0200

    Add files via upload

--- Cinema/src/Movie.java
+++ Cinema/src/Movie.java
@@ -1,5 +1,7 @@
 public class Movie {
 
+    private final int capacity=20;
+    
     private int ID;
 
     private String name;
@@ -7,6 +9,10 @@ public class Movie {
     private boolean is3D;
 
     private Time timeOfPlay;
+    
+    //private static int movieCounter=0;
+    
+    private boolean nowPlaying;
 
     public boolean setMovieDetails(String name,int ID,boolean is3D,Time timeOfPlay)
     {
@@ -29,16 +35,67 @@ public class Movie {
         
             this.is3D=is3D;
             
-            if(ID>0)
+            if(ID>0&&ID<capacity)
             {
                 this.ID=ID;
             }
+            else{
+                return false;
+            }
         return true;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIs3D(boolean is3D) {
+        this.is3D = is3D;
+    }
+
+    public void setTimeOfPlay(int hour,int min) {
+        this.timeOfPlay.setTime(hour, min);
+    }
+
+    public void setNowPlaying(boolean nowPlaying) {
+        this.nowPlaying = nowPlaying;
     }
     Movie()
     {
-        ID=0;
-        name="NULL";
+        
+        
+            nowPlaying=false;
+            ID=-1;
+           /* ID=movieCounter++;
+            if(movieCounter>=Now_Playing.cap*2)
+            {
+                movieCounter%=Now_Playing.cap*2;
+            }*/
+            name="NULL";
         is3D=false;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isIs3D() {
+        return is3D;
+    }
+
+    public Time getTimeOfPlay() {
+        return timeOfPlay;
+    }
+
+    public boolean isNowPlaying() {
+        return nowPlaying;
     }
 }
