f3dfdd043e2786cbaa07e5e88b669857fb8dd864
mesho3620 <44414933+mesho3620@users.noreply.github.com>
2018-10-25 01:31:34 +0200

    Add files via upload

---
+++ Hall.java
@@ -0,0 +1,34 @@
+public class Hall {
+
+    private final char capacityy='G';
+    private final int capacityx=10;
+
+    private Seat array_seats[][]=new Seat[capacityy][capacityx];
+
+    private int ID;
+    
+    Schedule sch[];
+
+    
+
+    public Seat[][] getArray_seats() {
+        return array_seats;
+    }
+    public Seat getseat(char group,int pos)
+    {
+        return array_seats[group][pos];
+    }
+    
+    Hall() 
+    {
+        for(char i='A';i<=capacityy;i++)
+        {
+            for(int j=0;j<capacityx;j++)
+            {
+                array_seats[i][j].setSeat(j,i);                           //UNCOMMENT
+            }
+        }
+    }
+    
+    
+}
