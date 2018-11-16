79924b7f7d792930dd690b46a06f8d5dc5e1fa8f
AlbertLouca <35044146+AlbertLouca@users.noreply.github.com>
2018-11-16 13:31:45 +0200

    Update Ticket.java

--- Cinema/src/Ticket.java
+++ Cinema/src/Ticket.java
@@ -1,6 +1,74 @@
 public class Ticket {
+private int hallID;
+private String moviename;
+private char seatgroup;
 
+    public char getSeatgroup() {
+        return seatgroup;
+    }
+
+    public void setSeatgroup(char seatgroup) {
+        this.seatgroup = seatgroup;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getMoviename() {
+        return moviename;
+    }
+
+    public void setMoviename(String moviename) {
+        this.moviename = moviename;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+private Time time;
+    public Ticket() {
+       hallID=0;
+       moviename="";
+   price=0;
+    }
+
+private int price;
     private int ID;
+    
 
-    private int Hall_ID;
+    public int getID() {
+        return ID;
+    }
+
+    public boolean setID(int ID) {
+        if (ID>=0)
+        {
+             ID++;
+        this.ID = ID;
+        return true;
+        }
+        else 
+             return false ;
+                    
+                
+       
+    }
+public Time Get_Time (Movie mov) throws Exception {
+                    return mov.getTimeOfPlay();
+}
+
+    public int getHallID() {
+        return hallID;
+    }
+
+    public void setHallID(int hallID) {
+        this.hallID = hallID;
+    }
+
+
 }
