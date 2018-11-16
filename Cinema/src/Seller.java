f2985d22e403aef30f911bdfbc531ada10625078
AlbertLouca <35044146+AlbertLouca@users.noreply.github.com>
2018-11-16 13:31:23 +0200

    Update Seller.java

--- Cinema/src/Seller.java
+++ Cinema/src/Seller.java
@@ -1,5 +1,14 @@
 public class Seller extends Reserver {
 
-    public void giveTicket() {
+    private Ticket One;
+    public Ticket SetTicket(Movie mov,int hallID,Payment pay,char seatgroup,Time tim) 
+    {
+         tim=mov.getTimeOfPlay();
+            pay.CalcPrice(mov, tim, seatgroup);
+        One.setMoviename(mov.getName());
+One.setHallID(hallID);
+One.setTime(tim);
+One.setSeatgroup(seatgroup);
+return One;
     }
 }
