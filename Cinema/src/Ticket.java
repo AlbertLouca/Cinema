ed9b8ba77ab4512643bc13c40afc77b5f1edbca5
Farouk00 <44416222+Farouk00@users.noreply.github.com>
2018-11-18 00:27:34 +0200

    Update Ticket.java

--- Cinema/src/Ticket.java
+++ Cinema/src/Ticket.java
@@ -7,30 +7,70 @@ private char seatgroup;
         return seatgroup;
     }
 
-    public void setSeatgroup(char seatgroup) {
-        this.seatgroup = seatgroup;
+    public boolean setSeatgroup(char seatgroup)
+    {
+        if (seatgroup >= 'A' && seatgroup <= 'G')
+        {
+         this.seatgroup = seatgroup;
+         return true;
+        }
+        else
+        {
+        return true;
+        }
     }
 
-    public void setTime(Time time) {
-        this.time = time;
+    public boolean setTime(int time)
+    {
+        if (time <= 0 && time >= 23)
+        {
+             this.time = time;
+            return true;
+        }
+        else 
+        {
+           
+            return false;
+        }
     }
 
     public String getMoviename() {
         return moviename;
     }
 
-    public void setMoviename(String moviename) {
-        this.moviename = moviename;
+    public boolean setMoviename(String moviename)
+    {
+        if (moviename.length()<=0 &&moviename.length()>=100)
+        {
+              this.moviename = moviename;
+            return true;
+        }
+        else
+        {
+        
+          return false;
+        }
+       
     }
+    
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public boolean setPrice(int price) 
+    {
+        if (price < 0 )
+        {
+            return false;
+        }
+        else 
+        {
+          this.price = price;
+        return true;
+        }
     }
-private Time time;
+private int time;
     public Ticket() {
        hallID=0;
        moviename="";
@@ -66,9 +106,18 @@ public Time Get_Time (Movie mov) throws Exception {
         return hallID;
     }
 
-    public void setHallID(int hallID) {
-        this.hallID = hallID;
+    public boolean setHallID(Hall hall) 
+    {
+        if (hallID<=0 && hallID >= 3)
+        {
+             this.hallID = hall.getID();
+            return true;
+        }
+        else 
+        {
+       
+        return false; 
+        }
     }
-
 
 }
