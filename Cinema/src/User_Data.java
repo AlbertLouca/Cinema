bd4009fd7995b068c3915f15f56fa91ab64cc0c2
AlbertLouca <35044146+AlbertLouca@users.noreply.github.com>
2018-11-25 22:41:02 +0200

    Update User_Data.java

--- Cinema/src/User_Data.java
+++ Cinema/src/User_Data.java
@@ -1,16 +1,17 @@
-
+package Users;
 import java.io.*;
+import cinema.*;
+import static cinema.Cinema.UserLib;
 import java.util.*;
-public class User_Data extends User implements IFile  {
- private ArrayList<User> ArrayOfusers =new ArrayList<User>();
+public class User_Data extends User implements IFile,Serializable  {
+ 
 
-@Override
          public boolean load()
    {
        try
        {         
-         ObjectInputStream Inp = new ObjectInputStream (new FileInputStream("C:\\Users\\PC\\Desktop\\Out.bin"));
-         ArrayOfusers = (ArrayList<User>) Inp.readObject();
+         ObjectInputStream Inp = new ObjectInputStream (new FileInputStream("C:\\Users\\Chadi N. Louca\\Documents\\Cinema\\Users Data.bin"));
+         UserLib = (ArrayList<User>) Inp.readObject();
 return true;
     } 
        catch (Exception e)
@@ -18,13 +19,13 @@ return true;
        return false;
    }
    }
-@Override
     public boolean save()
     {
            try {
-               ObjectOutputStream Out = new ObjectOutputStream (new FileOutputStream("C:\\Users\\PC\\Desktop\\Out.bin"));
-                   Out.writeObject(ArrayOfusers);
+               ObjectOutputStream Out = new ObjectOutputStream (new FileOutputStream("C:\\Users\\Chadi N. Louca\\Documents\\Cinema\\Users Data.bin"));
+                   Out.writeObject(UserLib);
                    Out.close();
+              
                    return true;
            } catch (Exception e)
            {
